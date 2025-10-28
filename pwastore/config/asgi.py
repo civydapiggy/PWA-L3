61c40d0f27f500240f92243eab1552217620aed9
civydapiggy <civyz586@gmail.com>
2025-10-28 10:18:40 -0600

    Use pwastore.config.settings in ASGI

--- pwastore/config/asgi.py
+++ pwastore/config/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# pwastore/config/asgi.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pwastore.config.settings')
 
 application = get_asgi_application()
