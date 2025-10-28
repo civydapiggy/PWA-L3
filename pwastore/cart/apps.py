3ff1c58a8b1fad8c70859afc9c91b6bc0997e7c4
civydapiggy <civyz586@gmail.com>
2025-10-28 14:46:35 -0600

    Fix CartConfig class and fully-qualified app configs

--- pwastore/cart/apps.py
+++ pwastore/cart/apps.py
@@ -1,5 +1,6 @@
+# pwastore/cart/apps.py
 from django.apps import AppConfig
 
-class StoreProductsConfig(AppConfig):
+class CartConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "pwastore.store_products"
+    name = "pwastore.cart"
